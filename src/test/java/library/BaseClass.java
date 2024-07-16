f4be0b8198a3dbcc972a180e5a7687fcaaa18519
Menna Hassan <h.menna85@gmail.com>
2024-07-16 06:38:05 +0300

    initialize the browser on chrome

--- src/test/java/library/BaseClass.java
+++ src/test/java/library/BaseClass.java
@@ -1,4 +1,30 @@
 package library;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
 public class BaseClass {
+    public static WebDriver driver;
+
+    @BeforeMethod
+    public void initializeBrowser() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
