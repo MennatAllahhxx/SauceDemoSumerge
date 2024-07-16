83c28cf1d99bf5129273317dd79b1b02d543e508
Menna Hassan <h.menna85@gmail.com>
2024-07-16 06:39:08 +0300

    removing unnecessary imports

--- src/test/java/library/BaseClass.java
+++ src/test/java/library/BaseClass.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.IOException;
-
 public class BaseClass {
     public static WebDriver driver;
 
